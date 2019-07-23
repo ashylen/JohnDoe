175cb130a9122be50af0c24dedaafea8f343e5fd
Ashylen <ashylen@o2.pl>
2019-07-23 22:00:50 +0200

    Updates

---
+++ precache-manifest.42217801393808ccc6b4a03bf6a6e526.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b0cdff000b2bbc9333d00572dc2b767",
+    "url": "/JohnDoe/static/media/discography-bg.6b0cdff0.png"
+  },
+  {
+    "revision": "d266e007099bfe386402",
+    "url": "/JohnDoe/static/css/main.4111bb0c.chunk.css"
+  },
+  {
+    "revision": "5d070c86516bccd0096a85bfcfb12d4e",
+    "url": "/JohnDoe/static/media/tours-bg.5d070c86.png"
+  },
+  {
+    "revision": "9a454a781e101020295b",
+    "url": "/JohnDoe/static/js/1.9a454a78.chunk.js"
+  },
+  {
+    "revision": "7d879104202288cb2b45",
+    "url": "/JohnDoe/static/js/runtime~main.7d879104.js"
+  },
+  {
+    "revision": "1c4001d30cfb5f891dcaace5b19283e4",
+    "url": "/JohnDoe/static/media/header-bg.1c4001d3.png"
+  },
+  {
+    "revision": "2d6443195632f5dafab1e340c9b57346",
+    "url": "/JohnDoe/static/media/header-logo.2d644319.png"
+  },
+  {
+    "revision": "d266e007099bfe386402",
+    "url": "/JohnDoe/static/js/main.d266e007.chunk.js"
+  },
+  {
+    "revision": "636fc433b5f41283f9097bd899442b20",
+    "url": "/JohnDoe/static/media/discography-button-1-bg.636fc433.png"
+  },
+  {
+    "revision": "2c4aea41a6dca8593a0cb472c5d42997",
+    "url": "/JohnDoe/static/media/discography-button-2-bg.2c4aea41.png"
+  },
+  {
+    "revision": "c12ed1368f0fccccf28629f031e78fd6",
+    "url": "/JohnDoe/static/media/discography-button-3-bg.c12ed136.png"
+  },
+  {
+    "revision": "ca390be160508a1fa04a6b8f54fc2f2a",
+    "url": "/JohnDoe/static/media/title-1-bg.ca390be1.png"
+  },
+  {
+    "revision": "9a454a781e101020295b",
+    "url": "/JohnDoe/static/css/1.1a25496e.chunk.css"
+  },
+  {
+    "revision": "42f5cc514554d5361f20815267b352b5",
+    "url": "/JohnDoe/index.html"
+  }
+];
